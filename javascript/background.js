7f1b3201d4217a70b8d007143e7b31e07500d1b1
stupiddude1234568909 <78235577+stupiddude1234568909@users.noreply.github.com>
2023-11-06 09:26:55 +0100

    tilføjede et billede til residenschlosss

--- javascript/background.js
+++ javascript/background.js
@@ -7,7 +7,7 @@ var backgroundImages = ["URL('billeder/Billeder til baggrund/Old Dresden.png')",
                         "URL('billeder/Golden rider og Karl Johann 1/IMG_2735.png')", // golden reither 
                         "URL('billeder/zwinger/20230920_145651.jpg')", //karl johan billede her 
                         "URL('billeder/zwinger/ZwingerMain.jpg')", //altstadt billede her 
-                        "URL('')", // Residenschloss
+                        "URL('billeder\Residenschloss\residenschloss.png')", // Residenschloss
                         "URL('billeder/zwinger/IMG20230920150447.jpg')" //zwinger
 ]
 
